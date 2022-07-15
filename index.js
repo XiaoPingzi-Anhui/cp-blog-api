6ac5ae4fb1e491cc1c972b19c0e8316dd48dfbd3
chenping3 <chenping3@finchina.com>
2022-07-15 14:40:39 +0800

    测试

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello Vercel';
-});
+// app.use(async ctx => {
+//   ctx.body = 'Hello Vercel';
+// });
+var express = require('express');
+var expressapp = express();
 
-app.listen(3008, () => {
+expressapp.get('/', function (req, res) {
+  res.send('Hello World');
+})
+
+expressapp.listen(3008, () => {
   console.log('3008项目启动')
 });

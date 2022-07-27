1d3f0922c3884356db4b696567d8a8926c8be7c7
chenping <1915375353@qq.com>
2022-07-27 20:38:43 +0800

    建立表

---
+++ mongoose/article/article.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const ArticleSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("article", ArticleSchema);
--- mongoose/user/user.model.js
+++ mongoose/user/user.model.js
@@ -1,18 +1,39 @@
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: [
+        /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+        "The value of path {PATH} ({VALUE}) is not a valid email address.",
+      ],
+    },
+    mobileNumber: {
+      type: String,
+      required: true,
+      match: [
+        /^1[3|4|5|8][0-9]\\d{8}$/,
+        "The value of path {PATH} ({VALUE}) is not a valid mobile number.",
+      ],
+    },
+    permissions: {
+      type: number,
+      required: true,
+    },
+    avatarLink: String,
+    age: Number,
+    birthday: Date,
   },
-  mobileNumber: {
-    type: String,
-    required: true,
-    // match: [
-    //   /^[1-9][0-9]{9}$/,
-    //   "The value of path {PATH} ({VALUE}) is not a valid mobile number.",
-    // ],
-  },
-  age: Number,
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("user", UserSchema);

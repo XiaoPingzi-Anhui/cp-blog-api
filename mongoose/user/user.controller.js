3efcc46371e140d62a7ecfc9eaafdb6c21b41acf
chenping <1915375353@qq.com>
2023-04-22 10:49:09 +0800

    test

--- mongoose/user/user.controller.js
+++ mongoose/user/user.controller.js
@@ -94,8 +94,10 @@ function login(req, res) {
           message: "Invalid email or password!",
         });
       } else {
+        console.log("req:", req.cookie);
         res
           .status(200)
+          .setHeader("Access-Control-Allow-Origin", req.host)
           .setHeader("Set-Cookie", `access_token=${await jwt.signToken(user)}}`)
           .json(
             handleReturn({

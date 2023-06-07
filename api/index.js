6647b7d206de4d72177028843ff974864ee4d4b7
chenping3 <chenping3@finchina.com>
2023-06-07 15:08:54 +0800

    options预检

--- api/index.js
+++ api/index.js
@@ -4,12 +4,15 @@ const myApi = require("../mongoose/index");
 
 const app = express();
 app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'content-type,Authorization');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Content-Type', 'application/json;charset=utf-8');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'content-type,Authorization');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  if (req.method.toLowerCase() == 'options') {
+      res.send(200);  // 让options尝试请求快速结束
+  }
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
